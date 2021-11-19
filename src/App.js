85841bd2b027308b241fb6440abbf73fdf48c9bf
Chris Rider <csrider@gmail.com>
2021-11-19 10:22:41 +0900

    staging placeholder dummy data and elements

--- src/App.js
+++ src/App.js
@@ -2,7 +2,22 @@
 
 function App() {
   return (
-    <div>MNS Connections Mobile</div>
+    <div>
+      <div className="header-main">
+        <div id="title-main">MNS Connections Mobile</div>
+        <div id="menu-main">[-]</div>
+      </div>
+
+      <div className="header-msg-section">Favorites</div>
+      <div>
+        Sample msg
+      </div>
+
+      <div className="header-msg-section">Shared</div>
+      <div>
+        Sample msg
+      </div>
+    </div>
   );
 }
 
